40b1e42266ff18800c2d6ddaf5811e9fe4ce6c67
Darsht24 <151422529+Darsht24@users.noreply.github.com>
2024-01-14 16:24:04 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to Young Minds";
+		return "Thank you Deepak Bamne";
 	}
 
 	public static void main(String[] args) {
